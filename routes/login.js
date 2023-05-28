6a45e7c52aa844e0f5d78db7217cb6da361d63df
gozonjoedaimar <gozonjoedaimar@gmail.com>
2023-05-28 16:46:27 +0800

    Add google recaptcha

--- routes/login.js
+++ routes/login.js
@@ -1,14 +1,27 @@
 var express = require('express');
 var router = express.Router();
 var debug = require('debug')('tbmapidocs:route:login');
+var reCAPTCHA = require('google-recaptcha');
+
+var googleRecaptcha = new reCAPTCHA({
+  secret: process.env.RECAPTCHA_SECRET
+});
 
 /* GET login page. */
 router.get('/',function(req, res, next) {
-  res.render('login');
+  res.render('login', { title: "Login" });
 });
 
 router.post('/', function(req, res, next) {
-  res.redirect('/login/?login=test');
+  const recaptchaResponse = req.body['g-recaptcha-response'];
+
+  googleRecaptcha.verify({response: recaptchaResponse}, function(error) {
+    if (error) {
+      debug(error.message);
+      return res.redirect('/login/?login=error');
+    }
+    return res.redirect('/login/?login=success');
+  });
 })
 
 module.exports = router;
