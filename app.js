ddd0d724554f67d16f3e330a14cbf4e241adaa08
gozonjoedaimar <gozonjoedaimar@gmail.com>
2023-06-12 14:47:26 +0800

    Add system config and update error page

--- app.js
+++ app.js
@@ -91,7 +91,8 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = err;
+  // res.locals.error = req.app.get('env') === 'development' ? err : {};
   // res.locals.env = { dev: true }
 
   // render the error page
